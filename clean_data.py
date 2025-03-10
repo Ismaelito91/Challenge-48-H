45698c8b80b83debed5f3143272dee5aa5577b53
flisa <lisa.fedoul>
2025-03-10 15:40:31 +0100

    rajout du fichier pour nettoyer des données

--- clean_data.py
+++ clean_data.py
@@ -8,6 +8,7 @@ def load_data(file_path):
     df = pd.read_csv(file_path, sep=';', encoding='utf-8')
     return df
 
+
 #  Fonction pour supprimer les emojis
 def remove_emojis(text):
     if isinstance(text, str):
@@ -27,18 +28,16 @@ def clean_text(df):
     df['name'] = df['name'].where(df['name'].notna() & df['name'].str.strip().ne(''), df['screen_name'])
     return df
 
-# Créer deux nouvelles colonnes 'date' et 'heure' et convertir les dates
 def process_dates(df):
     df['created_at'] = pd.to_datetime(df['created_at'], errors='coerce', utc=True)
     df['date'] = df['created_at'].dt.strftime('%Y-%m-%d')
-    df['heure'] = df['created_at'].dt.strftime('%H:%M:%S')
     df = df.drop(columns=['created_at'])
     return df
 
-# Supprimer la colonne 'id' et créer une numérotation de 1 à N
 def process_ids(df):
-    df = df.drop(columns=['id'], errors='ignore')
-    df.insert(0, 'id', range(1, len(df) + 1))
+    if 'id' in df.columns:
+        df['id'] = df['id'].astype(str).str.replace(',', '.').astype(float, errors='ignore')
+        df['id'] = df['id'].apply(lambda x: f"{x:.2E}" if pd.notna(x) else x)
     return df
 
 
