45429ae90020d2886ade70af989d077196fcd2db
Ismael Cerezo <i.cerezo@actongroup.com>
2025-03-10 15:57:19 +0100

    Refine AI agent prompt for tweet sentiment and categorization

--- agent_ia.py
+++ agent_ia.py
@@ -13,14 +13,14 @@ with open('cleaned_tweets.csv', newline='', encoding='utf-8') as csvfile:
     tweets = [row[3] for row in csv_reader]
 
 # Use the 289 tweet as content for the example
-content = tweets[289] if tweets else ""
+content = tweets[89] if tweets else ""
 
 # Prompt the user with a system message and the content of the tweet
 message = [
-    {"role": "system", "content": "Tu vas recevoir des tweet de plainte et repond toujours dans le format suivant :"
-                                  "1. tu vas devoir les noter de 0 à 100, 100 étant la situation la plus grave et 0 la situation la moins grave."
-                                  "2. si le sentiment et plus positif, neutre ou négatif."
-                                  "3. Répartir les différrents tweet dans les catégories suivants :Problème de facturation, Panne et urgences, Service client injoignable, Probleme avec l'aplication, Delai d'intervention, Tweet sans importance particulier."
+    {"role": "system", "content": "Tu vas recevoir des tweets de plainte et répondre toujours dans le format suivant :"
+                                  "1. Tu vas devoir les noter de 0 à 100, 100 étant la situation la plus grave et 0 la situation la moins grave."
+                                  "2. Si le sentiment de l'utilisateur est plus positif, neutre ou négatif envers ENGIE. Réponds juste par positif, neutre ou négatif."
+                                  "3. Répartir les différrents tweet dans les catégories suivants :Problème de facturation, Panne et urgences, Service client injoignable, Probleme avec l'aplication, Delai d'intervention,  Problème d'age, Autre."
                                   "4. Petit description de la situation."},
     {"role": "user","content": content}
 ]
